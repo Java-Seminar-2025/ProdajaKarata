0c0bbdd4b3e45144eb333381c810118b746e1334
leakl <leakl@BALROG>
2025-10-31 13:47:04 +0100

    update

---
+++ src/main/java/com/football/ticketsale/FootballTicketSaleApplication.java
@@ -0,0 +1,12 @@
+package com.football.ticketsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTicketSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTicketSaleApplication.class, args);
+    }
+}
