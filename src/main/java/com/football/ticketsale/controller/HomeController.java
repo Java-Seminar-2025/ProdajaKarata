a7942509bb701cc3fe495ff6a578e0a1097c05ef
Marko Suša <marko3susa@gmail.com>
2026-01-26 12:46:08 +0100

    added settings and updated docker

--- src/main/java/com/football/ticketsale/controller/HomeController.java
+++ src/main/java/com/football/ticketsale/controller/HomeController.java
@@ -1,13 +1,11 @@
 package com.football.ticketsale.controller;
 
-import ch.qos.logback.core.util.StringUtil;
 import com.football.ticketsale.entity.UserEntity;
 import com.football.ticketsale.service.UserService;
-import com.football.ticketsale.service.UserService.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -33,13 +31,17 @@ public class HomeController {
     public String showHomePage(
             Model model, @AuthenticationPrincipal UserDetails userDetails,
             @RequestParam(name = "q", required = false) String q) {
+        if (userDetails == null) {
+            return "redirect:/login";
+        }
         String username = userDetails.getUsername();
-
         UserEntity fullUser = userService.findUserByUsername(username);
 
-        model.addAttribute("username", username);
-        model.addAttribute("fullName", fullUser.getFullName());
-        model.addAttribute("email", fullUser.getEmail());
+        if (fullUser == null) {
+            return "redirect:/logout";
+        }
+
+        model.addAttribute("user", fullUser);
         model.addAttribute("role", StringUtils.capitalize(fullUser.getAuthorizationLevel()));
 
         List<MatchEntity> matches = matchService.getUpcomingMatches(q);
@@ -49,4 +51,3 @@ public class HomeController {
         return "home";
     }
 }
-
