037c34a8843f438f06a51456edd33544f64ec13b
Marko Suša <marko3susa@gmail.com>
2026-01-26 01:18:50 +0100

    added matches to the dashboard and purchases
    # Conflicts:
    #	src/main/java/com/football/ticketsale/config/SecurityConfig.java
    #	src/main/java/com/football/ticketsale/controller/HomeController.java
    #	src/main/resources/templates/home.html

--- src/main/java/com/football/ticketsale/controller/HomeController.java
+++ src/main/java/com/football/ticketsale/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.football.ticketsale.controller;
 
+import ch.qos.logback.core.util.StringUtil;
 import com.football.ticketsale.entity.UserEntity;
 import com.football.ticketsale.service.UserService;
+import com.football.ticketsale.service.UserService.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -31,17 +33,13 @@ public class HomeController {
     public String showHomePage(
             Model model, @AuthenticationPrincipal UserDetails userDetails,
             @RequestParam(name = "q", required = false) String q) {
-        if (userDetails == null) {
-            return "redirect:/login";
-        }
         String username = userDetails.getUsername();
+
         UserEntity fullUser = userService.findUserByUsername(username);
 
-        if (fullUser == null) {
-            return "redirect:/logout";
-        }
-
-        model.addAttribute("user", fullUser);
+        model.addAttribute("username", username);
+        model.addAttribute("fullName", fullUser.getFullName());
+        model.addAttribute("email", fullUser.getEmail());
         model.addAttribute("role", StringUtils.capitalize(fullUser.getAuthorizationLevel()));
 
         List<MatchEntity> matches = matchService.getUpcomingMatches(q);
@@ -51,3 +49,4 @@ public class HomeController {
         return "home";
     }
 }
+
